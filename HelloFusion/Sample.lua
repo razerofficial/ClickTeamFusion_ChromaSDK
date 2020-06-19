21c0a171d2813e57be1ee3759ce1a048530e6d15
Timothy Graupmann <tim.graupmann@razer.com>
2020-06-19 14:12:22 -0700

    Experimenting with lua

--- HelloFusion/Sample.lua
+++ HelloFusion/Sample.lua
@@ -1,4 +1,8 @@
+ChromaAnimationAPI = {}
+function ChromaAnimationAPI.playAnimationName(path, loop)
+  print("playAnimationName: " .. path);
+end
 function showEffect1(var)
-  print("Hello")
-return "Ran effect: " .. var;
+  ChromaAnimationAPI.playAnimationName("Animations/Spiral_Keyboard.chroma", true)
+return ("Ran effect: " .. var)
 end
