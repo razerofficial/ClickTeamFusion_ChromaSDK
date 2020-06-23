6220ef3cf732bf8aca170c597309bf3dc39aa049
Timothy Graupmann <tim.graupmann@razer.com>
2020-06-23 10:36:04 -0700

    Wrapped C functions in an object

--- HelloFusion/Sample.lua
+++ HelloFusion/Sample.lua
@@ -1,21 +1,20 @@
 function ShowEffect1()
-  --ChromaAnimationAPI.PlayAnimationName("Animations/Spiral_Keyboard.chroma", true)
-  PlayAnimationName("Animations/Rainbow_Keyboard.chroma", true)
+  ChromaAnimationAPI.PlayAnimationName("Animations/Rainbow_Keyboard.chroma", true)
 end
 function ShowEffect1ChromaLink()
-  PlayAnimationName("Animations/Rainbow_ChromaLink.chroma", true)
+  ChromaAnimationAPI.PlayAnimationName("Animations/Rainbow_ChromaLink.chroma", true)
 end
 function ShowEffect1Headset()
-  PlayAnimationName("Animations/Rainbow_Headset.chroma", true)
+  ChromaAnimationAPI.PlayAnimationName("Animations/Rainbow_Headset.chroma", true)
 end
 function ShowEffect1Keypad()
-  PlayAnimationName("Animations/Rainbow_Keypad.chroma", true)
+  ChromaAnimationAPI.PlayAnimationName("Animations/Rainbow_Keypad.chroma", true)
 end
 function ShowEffect1Mousepad()
-  PlayAnimationName("Animations/Rainbow_Mousepad.chroma", true)
+  ChromaAnimationAPI.PlayAnimationName("Animations/Rainbow_Mousepad.chroma", true)
 end
 function ShowEffect1Mouse()
-  PlayAnimationName("Animations/Rainbow_Mouse.chroma", true)
+  ChromaAnimationAPI.PlayAnimationName("Animations/Rainbow_Mouse.chroma", true)
 end
 
 function ExecuteEffect(index)
