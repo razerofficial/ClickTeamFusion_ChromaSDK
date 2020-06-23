e2724e21708b40ac0aa5c303af0a55cfb796f66b
Timothy Graupmann <tim.graupmann@razer.com>
2020-06-23 10:05:17 -0700

    Integrated with Lua

--- HelloFusion/Sample.lua
+++ HelloFusion/Sample.lua
@@ -1,8 +1,30 @@
-ChromaAnimationAPI = {}
-function ChromaAnimationAPI.playAnimationName(path, loop)
-  print("playAnimationName: " .. path);
+function ShowEffect1()
+  --ChromaAnimationAPI.PlayAnimationName("Animations/Spiral_Keyboard.chroma", true)
+  PlayAnimationName("Animations/Rainbow_Keyboard.chroma", true)
 end
-function showEffect1(var)
-  ChromaAnimationAPI.playAnimationName("Animations/Spiral_Keyboard.chroma", true)
-return ("Ran effect: " .. var)
+function ShowEffect1ChromaLink()
+  PlayAnimationName("Animations/Rainbow_ChromaLink.chroma", true)
+end
+function ShowEffect1Headset()
+  PlayAnimationName("Animations/Rainbow_Headset.chroma", true)
+end
+function ShowEffect1Keypad()
+  PlayAnimationName("Animations/Rainbow_Keypad.chroma", true)
+end
+function ShowEffect1Mousepad()
+  PlayAnimationName("Animations/Rainbow_Mousepad.chroma", true)
+end
+function ShowEffect1Mouse()
+  PlayAnimationName("Animations/Rainbow_Mouse.chroma", true)
+end
+
+function ExecuteEffect(index)
+  if index == 1 then
+    ShowEffect1()
+    ShowEffect1ChromaLink()
+    ShowEffect1Headset()
+    ShowEffect1Keypad()
+    ShowEffect1Mousepad()
+    ShowEffect1Mouse()
+  end
 end
